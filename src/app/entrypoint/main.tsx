0470ac3f21b122d94adc1a8306ab8a9e3ff4a4a1
kmyagkov <kirillmyagkov17@gmail.com>
2025-10-10 14:40:12 +0300

    Add tools

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router' 
 import '../styles/index.css'
-import { router } from '../routes';
+import { router } from '@/app/routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
